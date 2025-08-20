18931703964a338612d0ca9ecb11dc9ed38fedd9
Arca <wu.xinhai@outlook.com>
2025-08-20 15:22:36 +0800

    创建一个地球

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>hello world!你好</div>
+  <div id="cesiumContainer"></div>
 </template>
+
+<script setup>
+import { Viewer } from 'cesium'
+import { flyTo, addCameraPositionLogger, cesiumConfig, addEntity } from '@/utils/cesium'
+import { onMounted } from 'vue'
+
+let viewer
+
+const initMap = () => {
+  viewer = new Viewer('cesiumContainer', cesiumConfig)
+}
+onMounted(() => {
+  initMap()
+  addEntity(viewer)
+  flyTo(viewer)
+  addCameraPositionLogger(viewer)
+})
+</script>
---
+++ src/utils/cesium.js
@@ -0,0 +1,60 @@
+import * as Cesium from 'cesium'
+const cesiumConfig = {
+  homeButton: false,
+  sceneModePicker: false,
+  fullscreenButton: false,
+  infoBox: false,
+  selectionIndicator: false,
+  baseLayerPicker: false,
+  shadows: true,
+  shouldAnimate: true,
+  animation: false,
+  timeline: false,
+  geocoder: false,
+  navigationHelpButton: false,
+  contextOptions: {
+    contextType: 2,
+  },
+  creditContainer: document.createElement('div'),
+}
+
+function addCameraPositionLogger(viewer) {
+  viewer.camera.moveEnd.addEventListener(() => {
+    const cartesianPosition = viewer.camera.position
+    const cartographicPosition = Cesium.Cartographic.fromCartesian(cartesianPosition)
+
+    if (cartographicPosition) {
+      const longitude = Cesium.Math.toDegrees(cartographicPosition.longitude)
+      const latitude = Cesium.Math.toDegrees(cartographicPosition.latitude)
+      const height = Cesium.Math.toDegrees(cartographicPosition.height)
+
+      console.log(
+        `当前位置：经度=${longitude.toFixed(6)}, 纬度=${latitude.toFixed(6)}, 高度=${height.toFixed(
+          2
+        )}米`
+      )
+    }
+  })
+}
+function flyTo(viewer) {
+  viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(117.274783, 31.817392, 5000),
+    orientation: {
+      heading: Cesium.Math.toRadians(0),
+      pitch: Cesium.Math.toRadians(-60.0),
+    },
+  })
+}
+function addEntity(viewer) {
+  viewer.entities.add({
+    position: Cesium.Cartesian3.fromDegrees(117.27, 31.83, 3000),
+    box: {
+      dimensions: new Cesium.Cartesian3(100, 100, 100),
+      material: Cesium.Color.RED.withAlpha(0.5),
+      outline: true,
+      outlineColor: Cesium.Color.BLACK.withAlpha(0.5),
+    },
+  })
+}
+
+export { addCameraPositionLogger, flyTo, addEntity, cesiumConfig }
